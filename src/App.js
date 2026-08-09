85727289dd5ff5f1ed37d797f78d1b52cdc91114
Sumit <sumit90asa@gmail.com>
2026-08-09 14:47:57 +0530

    update on netfleix page

--- src/App.js
+++ src/App.js
@@ -8,47 +8,42 @@ import Latest from "./pages/Latest";
 import MyList from "./pages/MyList";
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <div className="bg-dark text-white" style={{ minHeight: '100vh' }}>
-        
-        {/* Navigation Bar */}
-        <Navbar bg="dark" data-bs-theme="dark" expand="lg" fixed="top" className="px-4">
-          <Container fluid>
-            <Navbar.Brand as={Link} to="/" style={{ color: '#E50914', fontWeight: 'bold', fontSize: '1.8rem', letterSpacing: '1px' }}>
-              NETFLIX
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link as={Link} to="/">Home</Nav.Link>
-                <Nav.Link as={Link} to="/tv-shows">TV Shows</Nav.Link>
-                <Nav.Link as={Link} to="/movies">Movies</Nav.Link>
-                <Nav.Link as={Link} to="/latest">Latest</Nav.Link>
-                <Nav.Link as={Link} to="/my-list">My List</Nav.Link>
-              </Nav>
-              <Nav>
-                <Nav.Link href="#profile">
-                  <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Profile" style={{ width: '32px', borderRadius: '4px' }} />
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+function App(){
+  return(
+  <Router>
+    <div className="bg-dark text white" style={{minHeight:'100vh'}}>
+      <Navbar bg="dark" data-bs-theme="dark" expand="lg" fixed="top" className="px-4">
+        <Container fluid>
+          <Navbar.Brand as={Link} to="/" style={{ color: '#E50914', fontWeight: 'bold', fontSize: '1.8rem', letterSpacing: '1px' }}>
+          NETFLIX
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/tv-shows">Tv Shows</Nav.Link>
+              <Nav.Link as={Link} to="/movies">Movies</Nav.Link>
+              <Nav.Link as={Link} to="/latest">Latest</Nav.Link>
+              <Nav.Link as={Link} to="/my-list">My List</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#profile">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Profile" style={{ width: '32px', borderRadius: '4px' }}/>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-        {/* Page Routes */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tv-shows" element={<TVShows />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/latest" element={<Latest />} />
-          <Route path="/my-list" element={<MyList />} />
-        </Routes>
-
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/tv-shows" element={<TVShows/>}/>
+        <Route path="/movies" element={<Movies/>}/>
+        <Route path="/latest" element={<Latest/>}/>
+        <Route path="/my-list" element={<MyList/>}/>
+      </Routes>
       </div>
-    </Router>
-  );
-}
-
+      </Router>
+  )
+};
 export default App;
